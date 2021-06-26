811df95ec1bee9e5849ea608e757e91a7db2a401
Landon Smothers <landonsmothers24@gmail.com>
2021-06-26 02:16:58 -0400

    made api display pictures

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${searchTerm}&api_key=E94wh5dgBxoBS27g84A8N9pOK3TWtS8HBBXLmhm1`);
-console.log(res.data);
+console.log();
+const img = document.createElement('IMG');
+img.src = res.data.photos [0] .img_src;
+document.body.append(img)
 })
